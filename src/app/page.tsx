30e85fb0a087322498a4ba55e7205552f5c4d007
Raj Mhatre <raj.m@sankeysolutions.com>
2025-12-27 23:32:12 -0600

    add: shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white dark:bg-black">
       <h1 className="text-4xl font-semibold text-black dark:text-white">
         OM Graphics and Film
       </h1>
+      <Button> Click Me </Button>
     </div>
   );
 }
